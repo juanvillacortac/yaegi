9db7ebf046c1cd0718da3357e41f9df1e5ba5887
Juan Villacorta <juanvillacortac@gmail.com>
2024-07-18 01:29:26 -0400

    chore: gen_symbols

--- stdlib/go1_20_go_ast.go
+++ stdlib/go1_20_go_ast.go
@@ -135,12 +135,8 @@ type _go_ast_Decl struct {
 	WPos   func() token.Pos
 }
 
-func (W _go_ast_Decl) End() token.Pos {
-	return W.WEnd()
-}
-func (W _go_ast_Decl) Pos() token.Pos {
-	return W.WPos()
-}
+func (W _go_ast_Decl) End() token.Pos { return W.WEnd() }
+func (W _go_ast_Decl) Pos() token.Pos { return W.WPos() }
 
 // _go_ast_Expr is an interface wrapper for Expr type
 type _go_ast_Expr struct {
@@ -149,12 +145,8 @@ type _go_ast_Expr struct {
 	WPos   func() token.Pos
 }
 
-func (W _go_ast_Expr) End() token.Pos {
-	return W.WEnd()
-}
-func (W _go_ast_Expr) Pos() token.Pos {
-	return W.WPos()
-}
+func (W _go_ast_Expr) End() token.Pos { return W.WEnd() }
+func (W _go_ast_Expr) Pos() token.Pos { return W.WPos() }
 
 // _go_ast_Node is an interface wrapper for Node type
 type _go_ast_Node struct {
@@ -163,12 +155,8 @@ type _go_ast_Node struct {
 	WPos   func() token.Pos
 }
 
-func (W _go_ast_Node) End() token.Pos {
-	return W.WEnd()
-}
-func (W _go_ast_Node) Pos() token.Pos {
-	return W.WPos()
-}
+func (W _go_ast_Node) End() token.Pos { return W.WEnd() }
+func (W _go_ast_Node) Pos() token.Pos { return W.WPos() }
 
 // _go_ast_Spec is an interface wrapper for Spec type
 type _go_ast_Spec struct {
@@ -177,12 +165,8 @@ type _go_ast_Spec struct {
 	WPos   func() token.Pos
 }
 
-func (W _go_ast_Spec) End() token.Pos {
-	return W.WEnd()
-}
-func (W _go_ast_Spec) Pos() token.Pos {
-	return W.WPos()
-}
+func (W _go_ast_Spec) End() token.Pos { return W.WEnd() }
+func (W _go_ast_Spec) Pos() token.Pos { return W.WPos() }
 
 // _go_ast_Stmt is an interface wrapper for Stmt type
 type _go_ast_Stmt struct {
@@ -191,12 +175,8 @@ type _go_ast_Stmt struct {
 	WPos   func() token.Pos
 }
 
-func (W _go_ast_Stmt) End() token.Pos {
-	return W.WEnd()
-}
-func (W _go_ast_Stmt) Pos() token.Pos {
-	return W.WPos()
-}
+func (W _go_ast_Stmt) End() token.Pos { return W.WEnd() }
+func (W _go_ast_Stmt) Pos() token.Pos { return W.WPos() }
 
 // _go_ast_Visitor is an interface wrapper for Visitor type
 type _go_ast_Visitor struct {
@@ -204,6 +184,4 @@ type _go_ast_Visitor struct {
 	WVisit func(node ast.Node) (w ast.Visitor)
 }
 
-func (W _go_ast_Visitor) Visit(node ast.Node) (w ast.Visitor) {
-	return W.WVisit(node)
-}
+func (W _go_ast_Visitor) Visit(node ast.Node) (w ast.Visitor) { return W.WVisit(node) }
